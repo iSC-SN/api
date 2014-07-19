c255e1a7766b5bbbf8e20992b6c31a2bc5a88b35
Suriyaa Kudo <isc.suriyaa@gmail.com>
2014-07-19 12:49:27 +0200

    Create index.php

---
+++ objects/debug/index.php
@@ -0,0 +1,37 @@
+API Object Debugger
+====
+
+* Input a URL / Access Token / ID = ____
+
+* Scrape Information
+** Response Code = 
+** Fetched URL = 
+** Canonical URL = 
+
+* Object Properties
+** ad:url = 
+** ad:type = 
+** ad:title = 
+** ad:locale = 
+** ad:image = 
+** ad:description = 
+** ad:site_name	= 
+** ad:updated_time = 
+
+* Share Preview
+
+* Raw Open Graph Document Information
+** Meta Tag =	<meta property="ad:url" content="" />
+** Meta Tag =	<meta property="ad:type" content="" />
+** Meta Tag =	<meta property="ad:title" content="" />
+** Meta Tag =	<meta property="ad:locale" content="" />
+** Meta Tag =	<meta property="ad:image" content="" />
+** Meta Tag =	<meta property="ad:description" content="" />
+** Meta Tag =	<meta property="ad:site_name" content="" />
+
+* URLs
+** Graph API =	https://data.sn.isc/ID
+** Scraped URL = 
+<?php
+Echo "<a href=https://developers.sn.isc/tools/debug/ad/echo?q=...>See exactly what our scraper sees for your URL</a>"
+ ?>
