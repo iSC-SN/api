8bafdcc48b70fba41c5e809f61fb3ae5f9de72db
Suriyaa Kudo <isc.suriyaa@gmail.com>
2014-07-19 12:37:14 +0200

    Create 0.php

---
+++ apps/0.php
@@ -0,0 +1,19 @@
+{
+   "id": "Number",
+   "created_time": "Year-Month-Day",
+   "description": "Text here!",
+   "image": [
+      {
+         "url": "Image Link"
+      }
+   ],
+   "is_scraped": true,
+   "locale": {
+      "locale": "lang_STATE"
+   },
+   "site_name": "Header 1",
+   "title": "Header 2",
+   "type": "website",
+   "updated_time": "Year-Month-Day",
+   "url": "http://www.example.com"
+}
